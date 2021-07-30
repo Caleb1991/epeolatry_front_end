1048e26444c6031f68571cd0b1966db0e24194f0
Caleb Wittman <calebjwittman1991@gmail.com>
2021-07-29 19:38:26 -0600

    Add fucntion to words api

--- app/services/words_api_services.rb
+++ app/services/words_api_services.rb
@@ -1,17 +1,27 @@
 class WordsApiServices
-  def self.get_users_words(user_id)
+  def self.get_users_words(user_id, page_number)
     response = Faraday.get('') do |req|
       req.params[''] = user_id
+      req.params[''] = page_number
       #another placeholder, may become unneccessary
     end
 
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.get_words_for_single_book(user_id, book_id)
+  def self.get_words_for_single_book(user_id, book_id, page_number)
     response = Faraday.get('') do |req|
       req.params[''] = user_id
       req.params[''] = book_id
+      req.params[''] = page_number
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_user_word_stats(user_id)
+    response = Faraday.get('') do |req|
+      req.params[''] = user_id
     end
 
     JSON.parse(response.body, symbolize_names: true)
