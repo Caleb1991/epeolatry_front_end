30cce9f3af111fd8519eda0bb7a81b49e29ac540
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-08-04 10:18:21 -0600

    Begin to test words index

--- spec/features/user/words/index_spec.rb
+++ spec/features/user/words/index_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'word index' do
+  describe 'visiting the page' do
+    it 'shows all of a users words' do
+      visit '/'
+    end
+
+  end
+
   xit 'word look up page' do
     user1 = User.create!(username: 'test', access_token: 'fake token', uid: '1233')
     VCR.use_cassette 'caterwaul search' do
