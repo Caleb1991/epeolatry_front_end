c3caaef0420341d2c7a1f3349d8ea590772c44c0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-04 14:13:52 -0700

    Update expect with proper content

--- spec/features/user/books/index_spec.rb
+++ spec/features/user/books/index_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe 'Books index page' do
   before :each do
     VCR.turn_on!
   end
-  xit 'shows all of a users books' do
+  it 'shows all of a users books' do
     VCR.turn_off!
     stub_request(:get, "https://epeolatry-back-end.herokuapp.com/api/v1/user/books?auth_token")
     .to_return(status: 200, body: File.open('./spec/fixtures/books_index.json').read, headers: {})
 
     visit user_books_path
 
-    expect(page).to have_content('lcpulzone@gmail.com')
+    expect(page).to have_content('Octavia E. Butler')
   end
 
   xit 'has the title as a link to the given books show page' do
