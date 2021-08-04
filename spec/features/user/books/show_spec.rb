9ff5abe009939002e6bb97641fc6cfc7310ac710
Aliya Merali <merali4@gmail.com>
2021-08-04 11:08:54 -0600

    Add test and fixure data for basic book show page

---
+++ spec/features/user/books/show_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'Book show page' do
+  before :each do
+    @user = User.create!(username: 'Bob@Boberton.bobmail.com', access_token: 'token', uid: 'thisismyid')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit user_dashboard_path(@user.uid)
+
+    @book_id = 'WxOFiAbwu_cC' #god of small things
+    @request_url = "https://epeolatry-back-end.herokuapp.com/api/v1/books/#{@book_id}?auth_token=#{@user.access_token}&user_id=auth_token=#{@user.uid}"
+  end
+
+  describe 'for a book in the reader\'s library' do
+    it 'shows the books key information' do
+      VCR.turn_off!
+      mock_return = File.open('./spec/fixtures/book_show_in_lib_no_words_fixture.json')
+      # stub_request(:get, "#{@request_url}")
+      #   .to_return(status: 200, body: mock_return, headers: {})
+
+      stub_request(:get, "https://epeolatry-back-end.herokuapp.com/api/v1/books/WxOFiAbwu_cC?auth_token=token&user_id=thisismyid").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.6.0'
+          }).
+        to_return(status: 200, body: mock_return, headers: {})
+
+      visit user_book_path(@book_id)
+      save_and_open_page
+      expect(page).to have_content("The Sparrow")
+      expect(page).to have_content("Mary Doria Russell")
+      expect(page).to have_content("Fiction")
+      expect(page).to have_content("A visionary work")
+      expect(page).to have_content("To Read")
+      expect(page).to have_content("Currently Reading")
+    end
+
+    it 'shows the words saved for book, where words link to show page and are listed in order of creation'
+    it 'if the book has no words associated, shows a note to add first word'
+    it 'has a search bar to look up a word for that book'
+  end
+
+  describe 'for a book not in the reader\'s library' do
+    it 'shows the books key information' do
+      #title
+      #author
+      #genres
+      #summary
+      # NO shelf
+    end
+    it 'shows the option to add that book to the reader\'s library'
+    it 'upon clicking the add-book button, redirects to book show page'
+    it 'upon clicking the add-book button, book show includes search bar to look up word'
+  end
+end
