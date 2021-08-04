6dfe3de6a170c9938e8d71c311f7b5c634d60ae2
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-03 17:09:53 -0700

    Add commented out method to books controller

---
+++ app/controllers/users/books_controller.rb
@@ -0,0 +1,27 @@
+class Users::BooksController < ApplicationController
+  def index
+    @books = UserFacade.get_my_books(session[:auth_token])
+  end
+
+  def show
+    @book = BookFacade.get_single_book(params[:id])
+    # @words = WordFacade.get_words_for_given_book(sessions[:user_id], params[:id])
+  end
+
+  def create
+    response = UserFacade.add_book_response(params[:id], sessions[:auth_token])
+    flash[:notice] = response
+    # redirect
+  end
+
+  # def create
+  #   response = UserFacade.create_glossary_response(params[:word], params[:book_id], session[:user_id])
+  #   flash[:notice] = response
+  # end
+
+  def destroy
+    response = UserFacade.remove_book_response(params[:id], sessions[:auth_token])
+    flash[:notice] = response
+    # redirect
+  end
+end
