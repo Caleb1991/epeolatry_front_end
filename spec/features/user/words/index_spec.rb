ed56bf719ba64a5799bd43040b5d659ff350e0ea
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-08-04 11:55:26 -0600

    I dont remeber

--- spec/features/user/words/index_spec.rb
+++ spec/features/user/words/index_spec.rb
@@ -1,23 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'word index' do
+  before :each do
+    user_1 = User.create!(
+      username: 'test@gmail.com', 
+      access_token: 'fake token', 
+      uid: '1233'
+    )
+  end
   describe 'visiting the page' do
     it 'shows all of a users words' do
-      visit '/'
-    end
-
-  end
-
-  xit 'word look up page' do
-    user1 = User.create!(username: 'test', access_token: 'fake token', uid: '1233')
-    VCR.use_cassette 'caterwaul search' do
+      stub_request(current_user)
+          .to_return(user_1)
       visit user_words_path
 
-      expect(page).to have_content("Quick Search")
-      fill_in :word, with: "caterwaul"
-      click_on "Search"
-      expect(current_path).to eq('/user/books/word/:word')
-      expect(page).to have_content('caterwaul')
+      expect(page).to have_content(user_1.username)
+
     end
   end
 end
+
+# xit 'word look up page' do
+#   user1 = User.create!(username: 'test', access_token: 'fake token', uid: '1233')
+#   VCR.use_cassette 'caterwaul search' do
+#     visit user_words_path
+
+#     expect(page).to have_content("Quick Search")
+#     fill_in :word, with: "caterwaul"
+#     click_on "Search"
+#     expect(current_path).to eq('/user/books/word/:word')
+#     expect(page).to have_content('caterwaul')
+#   end
+# end
