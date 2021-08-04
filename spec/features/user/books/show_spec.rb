f372657725eb450916b9d23fda1386f8d2c96837
Aliya Merali <merali4@gmail.com>
2021-08-04 15:17:38 -0600

    Add unlinked delete button to book show page

--- spec/features/user/books/show_spec.rb
+++ spec/features/user/books/show_spec.rb
@@ -72,6 +72,44 @@ RSpec.describe 'Book show page' do
       expect(page).to have_content("To Read")
       expect(page).to have_content("Currently Reading")
     end
+
+    it 'shows the option to remove that book to the reader\'s library' do
+      mock_return = File.open('./spec/fixtures/book_show_in_lib_with_words_fixture.json')
+      stub_request(:get, "https://epeolatry-back-end.herokuapp.com/api/v1/books/#{@book_id}?auth_token=#{@user.access_token}&user_id=#{@user.uid}")
+        .to_return(status: 200, body: mock_return, headers: {})
+
+      visit user_book_path(@book_id)
+      within("##{@book_id}-ToRead") do
+        expect(page).to have_button("Remove")
+      end
+      within("##{@book_id}-CurrentlyReading") do
+        expect(page).to have_button("Remove")
+      end
+    end
+
+    xit 'upon clicking the add-book button, redirects to book show page with word search bar' do
+      pre_mock_return = File.open('./spec/fixtures/book_show_not_in_lib_fixture.json')
+      stub_request(:get, "https://epeolatry-back-end.herokuapp.com/api/v1/books/#{@book_id}?auth_token=#{@user.access_token}&user_id=#{@user.uid}")
+        .to_return(status: 200, body: pre_mock_return, headers: {})
+
+      visit user_book_path(@book_id)
+
+      stub_request(:post, "https://epeolatry-back-end.herokuapp.com/api/v1/user/books?auth_token=token&shelf_id=2&volume_id=WxOFiAbwu_cC")
+        .to_return(status: 204, body: '{}', headers: {})
+
+      post_mock_return = File.open('./spec/fixtures/book_show_in_lib_no_words_fixture.json')
+      stub_request(:get, "https://epeolatry-back-end.herokuapp.com/api/v1/books/#{@book_id}?auth_token=#{@user.access_token}&user_id=#{@user.uid}")
+        .to_return(status: 200, body: post_mock_return, headers: {})
+
+      click_button("Add 'The Sparrow' to my 'To Read' Shelf")
+
+      expect(page).to have_current_path(user_book_path(@book_id))
+      expect(page).to have_content("Current Shelves:")
+      expect(page).to have_content("To Read")
+      expect(page).to have_content("Your Words for")
+      expect(page).to have_content("No words looked up yet!")
+      expect(page).to have_field(:query)
+    end
   end
 
 
