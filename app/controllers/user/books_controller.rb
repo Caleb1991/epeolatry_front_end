5399877af59b2bdd62c6b2e8b17336202b165483
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-05 14:27:20 -0700

    Comment in pagy gem

--- app/controllers/user/books_controller.rb
+++ app/controllers/user/books_controller.rb
@@ -1,12 +1,11 @@
 class User::BooksController < ApplicationController
   def index
-    # if !params[:search].nil?
-      # @pagy, @books = pagy_array(UserFacade.get_my_books(params[:auth_token]))
-      # @searched_books = BookFacade.search_for_books(params[:search])
-    # else
-    #   @pagy, @books = pagy_array(UserFacade.get_my_books(session[:auth_token]))
-    # end
-    @books = UserFacade.get_my_books(current_user.access_token)
+    if !params[:search].nil?
+      @pagy, @books = pagy_array(UserFacade.get_my_books(current_user.access_token))
+      @searched_books = BookFacade.search_for_books(params[:search])
+    else
+      @pagy, @books = pagy_array(UserFacade.get_my_books(current_user.access_token))
+    end
   end
 
   def show
