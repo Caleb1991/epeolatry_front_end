7971fc70862bad7886fe87660ef42d1c56e9fc1b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-08-04 22:22:17 -0700

    Index fixes

--- app/services/user_api_services.rb
+++ app/services/user_api_services.rb
@@ -3,7 +3,7 @@ class UserApiServices
     response = conn.get('api/v1/user/books') do |req|
       req.params[:auth_token] = auth_token
     end
-    parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.add_a_book(volume_id, auth_token, shelf_id = 2)
@@ -33,10 +33,6 @@ class UserApiServices
     end
 
     response.status
-  end
-
-  def self.parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
   end
 
   private
