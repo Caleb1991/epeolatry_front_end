a56a6607e7f61bd8ff06b4fb81039900eb251da7
Alex Ferencz <alex.ferencz6@gmail.com>
2021-08-05 15:30:42 -0600

    Books words show

--- app/controllers/user/books/word_controller.rb
+++ app/controllers/user/books/word_controller.rb
@@ -2,7 +2,7 @@ class User::Books::WordController < ApplicationController
   def show
     @word = WordFacade.search(params[:word]).first
     @book = BookFacade.get_single_book(params[:book_id])
-    # @book = BookPoro.new(title: 'Title', id: params[:book_id])
+    # @word_books = WordBookFacade.get_books_for_given_word(current_user.uid, params[:word])
   end
 
   def new
