58f475d0f925329c04390d44131a5efa9a78adae
Alex Ferencz <alex.ferencz6@gmail.com>
2021-08-04 19:55:02 -0600

    Small change to books word show

--- app/controllers/user/books/word_controller.rb
+++ app/controllers/user/books/word_controller.rb
@@ -1,5 +1,5 @@
 class User::Books::WordController < ApplicationController
-  def show # Need to spruce up to work correctly for this route
+  def show
     @word = WordFacade.search(params[:word]).first
     # @book = BookFacade.get_single_book(params[:book_id])
     @book = BookPoro.new(title: 'Title', id: params[:book_id])
@@ -9,6 +9,4 @@ class User::Books::WordController < ApplicationController
     UserFacade.create_glossary_response(params[:word], current_user.uid, params[:book_id])
     redirect_to "/user/books/:book_id"
   end
-  #create glossary action with glossary facade
-  #create cancel button to redirect if no save wanted
 end
