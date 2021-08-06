a601496a70f19a8cd7528714c174cb255671013d
Aliya Merali <merali4@gmail.com>
2021-08-06 08:55:25 -0600

    Resolve test issues for word show
    Co-authored-by: Alex Ferencz 
    <45580970+Aferencz1987@users.noreply.github.com>

--- app/controllers/user/books/word_controller.rb
+++ app/controllers/user/books/word_controller.rb
@@ -1,13 +1,13 @@
 class User::Books::WordController < ApplicationController
   def show
     @word = WordFacade.search(params[:word]).first
-    @book = BookFacade.get_single_book(params[:book_id])
+    @book = BookFacade.get_single_book(params[:book_id], current_user.id, current_user.access_token)[:book]
     # @book = BookPoro.new(title: 'Title', id: params[:book_id])
   end
 
   def new
-    UserFacade.create_glossary_response(params[:word], current_user.uid, params[:book_id])
-    redirect_to "/user/books/:book_id"
+    UserFacade.create_glossary_response(params[:word], current_user.id, params[:book_id])
+    redirect_to "/user/books/#{params[:book_id]}"
   end
 
   def search
