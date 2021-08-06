e9477ce10b95fb53cf5de3a4f414ed9b91ef5b99
Aliya Merali <merali4@gmail.com>
2021-08-06 06:33:47 -0600

    Add conditional logic, update tests to show login only when no current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    session[:auth_token] = nil
+    # session[:auth_token] = nil
     redirect_to root_path, message: "You Have Been Logged Out"
   end
 
--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -11,10 +11,21 @@ RSpec.describe 'Welcome Page' do
       expect(page).to have_content("A word & book tracking app")
     end
 
-    it 'displays option to login through Google' do
+    it 'displays option to login through Google if user is not logged in' do
       expect(page).to have_link("Login with Google")
       # click_link("Login with Google")
       # expect(current_path).to eq('https://accounts.google.com/o/oauth2/auth/oauthchooseaccount')
+    end
+
+    it 'if user is already logged in, shows logout option and no log in' do
+      @user_1 = User.create!(username: 'merali4@test.com', access_token: '1234', uid: '1234')
+      WelcomeController.any_instance.stub(:current_user).and_return(@user_1)
+
+      visit root_path
+
+      expect(page).to have_content("merali4")
+      expect(page).to have_content("Log Out")
+      expect(page).to_not have_link('Login with Google')
     end
 
   end
